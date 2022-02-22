129a9444f02d6852002807369df8e518be04e4a0
Pixel King <92705350+PixelDev990@users.noreply.github.com>
2022-02-21 21:56:21 -0600

    API Updated to have ladders
    Yes its bad to copy code but this is alpha and its only for testing.

---
+++ ToppatAPI-Bepinex/Models/MapType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToppatAPI.Models
+{
+    enum MapType
+    {
+        Skeld,
+        Mira,
+        Polus,
+        Airship
+    }
+}
